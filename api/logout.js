4f871824c400afd4e7f4ed63412e6020df0bc9aa
TanzManuel <beenelprat1@gmail.com>
2026-08-28 19:34:02 +0700

    Add files via upload

---
+++ api/logout.js
@@ -0,0 +1,2 @@
+import {json} from './_lib.js';
+export default async function handler(req,res){res.setHeader('Set-Cookie','admin_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');return json(res,200,{ok:true})}
